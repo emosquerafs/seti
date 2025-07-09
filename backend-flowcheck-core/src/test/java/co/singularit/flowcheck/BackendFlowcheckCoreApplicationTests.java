f68bbd282864e7f79770ead7fcaa720b3beaf1fb
Ernesto Luis Mosquera Heredia <ernesto.mosquera@singularit.co>
2025-07-09 09:21:11 -0500

    feat: initialize backend project structure with Spring Boot and configuration files

---
+++ backend-flowcheck-core/src/test/java/co/singularit/flowcheck/BackendFlowcheckCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package co.singularit.flowcheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendFlowcheckCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
